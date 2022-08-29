f45f6a424a2c928dc9ef0905423480369fd592fc
Michalzard <miso.platko@gmail.com>
2022-08-29 19:10:22 +0200

    scroll to top on every redirect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import "../src/styles/App.scss";
 import {
   Header,
@@ -12,12 +12,23 @@ import {
   About,
   FreshCoffee,
 } from "../src/components/index";
+import { useEffect } from "react";
+import ProductOrder from "./components/Products/ProductOrder";
+
+function ScrollToTop(){
+  //on every pathname change scroll to top to display important things
+  const {pathname} = useLocation();
+  useEffect(()=>{
+  document.body.scrollTo({top:0,left:0});
+  },[pathname]);
+  return null;
+}
 
 function App() {
   return (
     <div className="App">
       <Header />
-
+      <ScrollToTop/>
       <Routes>
         <Route
           path="/"
@@ -32,9 +43,15 @@ function App() {
           }
         />
 
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about-us" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="about-us" element={<About />} />
         <Route path="fresh-coffee" element={<FreshCoffee />} />
+        <Route path="my-account" element={<div style={{color:"red",fontSize:"30px"}}>Login Stuff</div>}/>
+
+        <Route path="product/:id"  element={<ProductOrder/>} />
+        
+
+        <Route path="*" element={<div style={{color:"red",fontSize:"30px"}}>NOT FOUND</div>} />
       </Routes>
 
       <Footer />
