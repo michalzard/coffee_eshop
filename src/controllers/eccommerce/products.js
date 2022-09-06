87f96db2091ae70bb024023f3195cfce595b7413
Michalzard <miso.platko@gmail.com>
2022-09-06 19:51:53 +0200

    try catch,for handling in asyncThunk

--- src/controllers/eccommerce/products.js
+++ src/controllers/eccommerce/products.js
@@ -1,10 +1,15 @@
 import commerce from "../../lib/commerce";
 export async function fetchAllProducts(productLimit) {
-  return await commerce.products
-    .list({ limit: productLimit })
-    .then((products) => {
-      const { data } = products;
-      return data;
-    })
-    .catch((err) => console.log(err));
+  try {
+    return await commerce.products
+      .list({ limit: productLimit })
+      .then((products) => {
+        const { data } = products;
+        return data;
+      })
+      .catch((err) => console.log(err));
+  } catch (err) {
+    const {message} = err.response.data;
+    return message;
+  }
 }
